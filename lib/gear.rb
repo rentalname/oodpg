aa7d83f5d12eb1c925373765ce1db550edafc393
rentalname <agenda21st@gmail.com>
2018-03-07 22:02:04 +0900

    add Gear#gear_inches

--- lib/gear.rb
+++ lib/gear.rb
@@ -1,15 +1,18 @@
 class Gear
-  attr_reader :chainring, :cog
+  attr_reader :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
   end
-end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
+end
