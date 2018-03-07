577dac9e8e6eed58ccfc2a1a0792ea20892da7d7
rentalname <agenda21st@gmail.com>
2018-03-07 22:23:14 +0900

    [from now] 2018/03/07 22:23:14
    diff --git a/lib/gear.rb b/lib/gear.rb
    index 3d52e0d..2a7f09c 100644
    --- a/lib/gear.rb
    +++ b/lib/gear.rb
    @@ -1,10 +1,10 @@
     class Gear
       attr_reader :chainring, :cog, :wheel
    
    -  def initialize(chainring, cog, rim, tire)
    +  def initialize(chainring, cog, wheel=nil)
         @chainring = chainring
         @cog = cog
    -    @wheel = Wheel.new(rim, tire)
    +    @wheel = wheel
       end
    
       def ratio
    diff --git a/lib/wheel.rb b/lib/wheel.rb
    new file mode 100644
    index 0000000..e70bdb1
    --- /dev/null
    +++ b/lib/wheel.rb
    @@ -0,0 +1,16 @@
    +class Wheel
    +  attr_reader :rim, :tire
    +
    +  def initialize(rim, tire)
    +    @rim = rim
    +    @tire = tire
    +  end
    +
    +  def diameter
    +    rim + (tire * 2)
    +  end
    +
    +  def circumference
    +    diameter * Math::PI
    +  end
    +end
    diff --git a/main.rb b/main.rb
    index db5de21..96aef6c 100644
    --- a/main.rb
    +++ b/main.rb
    @@ -1,4 +1,9 @@
     require_relative 'lib/gear'
    +require_relative 'lib/wheel'
    
    -puts Gear.new(52, 11, 26, 1.5).gear_inches
    -puts Gear.new(52, 11, 24, 1.25).gear_inches
    +wheel = Wheel.new(26, 1.5)
    +puts wheel.circumference
    +
    +puts Gear.new(52, 11, wheel).gear_inches
    +
    +puts Gear.new(52, 11).ratio

--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
 require_relative 'lib/gear'
+require_relative 'lib/wheel'
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+wheel = Wheel.new(26, 1.5)
+puts wheel.circumference
+
+puts Gear.new(52, 11, wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
